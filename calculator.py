d374aab52880e8aaa80bb99e43ef426e65848a3b
Omar Ben Salah <omar.ben-salah@polymtl.ca>
2021-04-18 11:41:25 -0700

    replaced angle of attack w engine power (again)

--- calculator.py
+++ calculator.py
@@ -6,15 +6,15 @@ import errno
 
 altitude = 1200
 avionicsUnit = "CHANGEMENT_ALT"
-angleOfAttack = 50
+enginePower = 50
 verticalSpeed = 1500
 
 requestedAltitude = 0
-requestedAngleOfAttack = 0
+requestedEnginePower = 0
 requestedVerticalSpeed = 0
 
 decodedAltitude = 0
-decodedAngleOfAttack = 0
+decodedEnginePower = 0
 decodedVerticalSpeed = 0
 
 def handleNewRequestedValues(signum, frame):
@@ -33,18 +33,18 @@ def handleNewRequestedValues(signum, frame):
 
             if (len(dataTab[0]) == 10): #A429
                 decodedAltitude = decodeA429(dataTab[0], "agr")["altitude"]
-                decodedAngleOfAttack =  decodeA429(dataTab[1], "agr")["verticalSpeed"]
-                decodedVerticalSpeed = decodeA429(dataTab[2], "agr")["angleOfAttack"]
+                decodedEnginePower =  decodeA429(dataTab[1], "agr")["verticalSpeed"]
+                decodedVerticalSpeed = decodeA429(dataTab[2], "agr")["enginePower"]
             else:
                 decodedAltitude = decodeAfdx(dataTab[0], "agr")["altitude"]
-                decodedAngleOfAttack =  decodeAfdx(dataTab[1], "agr")["verticalSpeed"]
-                decodedVerticalSpeed = decodeAfdx(dataTab[2], "agr")["angleOfAttack"]
+                decodedEnginePower =  decodeAfdx(dataTab[1], "agr")["verticalSpeed"]
+                decodedVerticalSpeed = decodeAfdx(dataTab[2], "agr")["enginePower"]
             
             requestedAltitude = int(decodedAltitude) if decodedAltitude != "" else 0
-            requestedAngleOfAttack = int(decodedAngleOfAttack) if decodedAngleOfAttack != "" else 0
+            requestedEnginePower = int(decodedEnginePower) if decodedEnginePower != "" else 0
             requestedVerticalSpeed = int(decodedVerticalSpeed) if decodedVerticalSpeed != "" else 0
 
-            print("New requested values are {}, {}, {}".format(requestedAltitude, requestedAngleOfAttack,requestedVerticalSpeed))
+            print("New requested values are {}, {}, {}".format(requestedAltitude, requestedEnginePower,requestedVerticalSpeed))
 
 signal.signal(signal.SIGUSR1, handleNewRequestedValues)
 
@@ -52,17 +52,17 @@ while True:
     with open('/tmp/calculatorToInterface', 'w') as f:
         if (random.uniform(0, 1) > 0.25):
             altitude += 100
-            angleOfAttack += 0.1
+            enginePower += 0.1
             verticalSpeed += 100
             avionicsUnit = "AU_SOL"
         else:
             altitude -= 100
-            angleOfAttack -= 0.1
+            enginePower -= 0.1
             verticalSpeed -= 100
             avionicsUnit = "CHANGEMENT_ALT"
 
-        A429 = "{},{},{}".format(encodeA429("cal", 1, avionicsUnit, altitude), encodeA429("cal", 2, "", verticalSpeed), encodeA429("cal", 3, "", angleOfAttack))
-        Afdx = "{},{},{}".format(encodeAfdx("cal", 1, avionicsUnit, altitude), encodeAfdx("cal", 2, "", verticalSpeed), encodeAfdx("cal", 3, "", angleOfAttack))
+        A429 = "{},{},{}".format(encodeA429("cal", 1, avionicsUnit, altitude), encodeA429("cal", 2, "", verticalSpeed), encodeA429("cal", 3, "", enginePower))
+        Afdx = "{},{},{}".format(encodeAfdx("cal", 1, avionicsUnit, altitude), encodeAfdx("cal", 2, "", verticalSpeed), encodeAfdx("cal", 3, "", enginePower))
 
         if (random.uniform(0, 1) > 0.5):
             f.write(A429 + "," + Afdx)
--- unitTests.py
+++ unitTests.py
@@ -20,8 +20,8 @@ print("Vertical Speed count {}".format(count))
 
 
 for i in range(-16, 16):
-    if decodeA429(encodeA429("agr",3,"CHANGEMENT_ALT", i),"agr")["angleOfAttack"] != i:
+    if decodeA429(encodeA429("agr",3,"CHANGEMENT_ALT", i),"agr")["enginePower"] != i:
         count += 1
         print("Error not Equal {}".format(i))
 
-print("Angle Of Attack count {}".format(count))
+print("Engine Power count {}".format(count))
